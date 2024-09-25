be9b90686f1ae746f68a6c49a2221b52dc694cf9
ibrahimmert <ibrahim.goze@crede-tech.com>
2024-09-25 16:34:17 +0300

    updateddd

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ export default function App() {
   return (
     <>
       <div className="h-screen w-full">
-        <div style={{ height: "10%" }} className="w-full  ">
+        <div style={{ height: "10%" }} className="w-full h-full  ">
           <NavBar></NavBar>
         </div >
 
-        <div style={{ height: "90%" }} className="flex flex-row">
+        <div style={{ height: "90%" }} className="flex flex-col p-4">
 
 
           <BottomSide></BottomSide>
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-gray-600  p-6">
-      <div className="container mx-auto x justify-center items-center">
-        <h1 className="text-white text-lg font-bold items-center justify-center ">
-          Credence Yetkilendirme Paneli
+    <nav className=" bg-gradient-to-bl from-blue-900 to-green-500  p-5 h-full">
+      <div className="container mx-auto  justify-center items-center">
+        <h1 className="text-white text-2xl font-bold items-center justify-center text-center">
+          CREDENCE YETKİLENDİRME PANELİ
         </h1>
       </div>
     </nav>
